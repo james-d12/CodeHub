261010851cb9f51d5ef9c880c90ee1accc16fc68
james <james_d02@protonmail.com>
2025-04-08 21:08:48 +0100

    Aspire

---
+++ src/aspire/CodeHub.Aspire.AppHost/Program.cs
@@ -0,0 +1,9 @@
+using Aspire.Hosting;
+
+var builder = DistributedApplication.CreateBuilder(args);
+
+var api = builder.AddProject<Projects.CodeHub_Api>("api");
+builder.AddProject<Projects.CodeHub_Portal>("portal")
+    .WithReference(api);
+
+builder.Build().Run();
